d34ca242a722bb68943ed7cd9e41e83c21e6d70c
titosilva <jt.mat@hotmail.com>
2022-03-18 00:20:52 -0300

    feat: added leds to Exp4

--- Exp4/main.c
+++ Exp4/main.c
@@ -1,5 +1,11 @@
 #include <msp430.h> 
 
+// LEDS
+#define SET_RED_LED (P1OUT |= BIT0)
+#define RESET_RED_LED (P1OUT &= ~BIT0)
+#define SET_GREEN_LED (P4OUT |= BIT7)
+#define RESET_GREEN_LED (P4OUT &= ~BIT7)
+
 // FREQUENCIES (HZ)
 #define FREQ_C 264
 #define FREQ_D 297
@@ -32,12 +38,14 @@ unsigned int dist_to_buzzer_freq(unsigned int dist);
 
 // Distance
 unsigned int compute_dist(unsigned int echo_microsec);
+void display_distance_on_leds(unsigned int dist);
 // ------------------------------------------------------
 
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
+	config_leds();
 	config_timers();
 	__enable_interrupt();
 
@@ -75,9 +83,31 @@ void config_timers(void)
     P2DIR |= BIT5;
 }
 
+void config_leds(void)
+{
+    // Red led
+    P1SEL &= ~BIT0;
+    P1DIR |= BIT0;
+    RESET_RED_LED;
+
+    // Green led
+    P4SEL &= ~BIT7;
+    P4DIR |= BIT7;
+    RESET_GREEN_LED;
+}
+
 // Buzzer
 void buzzer_play(int frequency)
 {
+    if (frequency == 0) {
+        // Turn buzzer off
+        P2SEL &= ~BIT5;
+        P2OUT &= ~BIT5;
+        return;
+    }
+
+    // Turn buzzer on
+    P2SEL |= BIT5;
     volatile unsigned int wave_half_period = 500 / (frequency / 1000);
     TA2CCR0 = wave_half_period + wave_half_period;
     TA2CCR2 = wave_half_period;
@@ -85,18 +115,30 @@ void buzzer_play(int frequency)
 
 unsigned int dist_to_buzzer_freq(unsigned int dist)
 {
-    if (dist <= 0 || dist > 50000)
-    {
-       return 0;
-    }
-
-    return 5000 - ((dist >> 10) - 5) * 125;
+    return (dist > 50000 || dist < 5)? 0 : 5000 - ((dist >> 10) - 5) * 125;
 }
 
 // Distance
 unsigned int compute_dist(unsigned int echo_microsec)
 {
     return 17 * echo_microsec;
+}
+
+void display_distance_on_leds(unsigned int dist)
+{
+    if (dist > 50000) {
+        RESET_GREEN_LED;
+        RESET_RED_LED;
+    } else if (dist > 30000) {
+        RESET_GREEN_LED;
+        SET_RED_LED;
+    } else if (dist > 5000) {
+        SET_GREEN_LED;
+        RESET_RED_LED;
+    } else {
+        SET_GREEN_LED;
+        SET_RED_LED;
+    }
 }
 // ------------------------------------------------------
 
@@ -115,25 +157,36 @@ __interrupt void timer1_a1_interrupt(void)
     if (waiting_falling_edge) {
         falling_capture_ccr = TA1CCR1;
 
-        if (++quantity_of_measurements > MAX_MEASUREMENTS)
-        {
-            quantity_of_measurements = 1;
-        }
-
         volatile unsigned int dist = compute_dist(
                 (falling_capture_ccr > rising_capture_ccr)?
                 falling_capture_ccr - rising_capture_ccr :
                 0xFFFF + falling_capture_ccr - rising_capture_ccr
         );
 
+        if (dist > 60000) {
+            waiting_falling_edge = 0;
+            return;
+        }
+
+        if (++quantity_of_measurements > MAX_MEASUREMENTS)
+        {
+            quantity_of_measurements = 1;
+        }
+
         dist_last_measurements[quantity_of_measurements - 1] = dist;
 
-        volatile unsigned int i = 0, sum = 0;
+        volatile unsigned int i = 0;
+        volatile long sum = 0;
         for (i = 0; i < MAX_MEASUREMENTS; i++)
         {
             sum += dist_last_measurements[i];
         }
-        buzzer_play(dist_to_buzzer_freq(sum >> 4));
+
+        volatile unsigned int average_dist = sum >> 4;
+
+        buzzer_play(dist_to_buzzer_freq(average_dist));
+        display_distance_on_leds(average_dist);
+
         waiting_falling_edge = 0;
     }
 }
