e3119a4a44cb41ed9f7cfb47d10b283f40e0bcd6
titosilva <jt.mat@hotmail.com>
2022-03-17 23:35:12 -0300

    feat: working on Exp4

--- Exp4/main.c
+++ Exp4/main.c
@@ -17,8 +17,8 @@
 volatile int waiting_falling_edge = 0;
 volatile unsigned int rising_capture_ccr = 0;
 volatile unsigned int falling_capture_ccr = 0;
-volatile unsigned int echo_last_measurements[MAX_MEASUREMENTS];
-volatile int echo_quantity_of_measurements = 1;
+volatile unsigned int dist_last_measurements[MAX_MEASUREMENTS];
+volatile int quantity_of_measurements = 1;
 // ------------------------------------------------------
 
 // FUNCTION SIGNATURES ----------------------------------
@@ -26,11 +26,12 @@ volatile int echo_quantity_of_measurements = 1;
 void config_timers(void);
 void config_leds(void);
 
-// Timer control
+// Buzzer
 void buzzer_play(int frequency);
+unsigned int dist_to_buzzer_freq(unsigned int dist);
 
-// Utils
-int frequency_to_aclk_half_cycles(int frequency);
+// Distance
+unsigned int compute_dist(unsigned int echo_microsec);
 // ------------------------------------------------------
 
 int main(void)
@@ -74,18 +75,28 @@ void config_timers(void)
     P2DIR |= BIT5;
 }
 
-// Timer control
+// Buzzer
 void buzzer_play(int frequency)
 {
-    volatile int aclk_half_cycles = frequency_to_aclk_half_cycles(frequency);
-    TA2CCR0 = aclk_half_cycles + aclk_half_cycles;
-    TA2CCR2 = aclk_half_cycles;
+    volatile unsigned int wave_half_period = 500 / (frequency / 1000);
+    TA2CCR0 = wave_half_period + wave_half_period;
+    TA2CCR2 = wave_half_period;
 }
 
-// Utils
-int frequency_to_aclk_half_cycles(int frequency)
+unsigned int dist_to_buzzer_freq(unsigned int dist)
 {
-    return frequency >> 1;
+    if (dist <= 0 || dist > 50000)
+    {
+       return 0;
+    }
+
+    return 5000 - ((dist >> 10) - 5) * 125;
+}
+
+// Distance
+unsigned int compute_dist(unsigned int echo_microsec)
+{
+    return 17 * echo_microsec;
 }
 // ------------------------------------------------------
 
@@ -101,22 +112,29 @@ __interrupt void timer1_a1_interrupt(void)
         return;
     }
 
-    falling_capture_ccr = TA1CCR1;
+    if (waiting_falling_edge) {
+        falling_capture_ccr = TA1CCR1;
 
-    if (++echo_quantity_of_measurements > MAX_MEASUREMENTS)
-    {
-        echo_quantity_of_measurements = 1;
-    }
-    echo_last_measurements[echo_quantity_of_measurements - 1] =
-            (falling_capture_ccr < rising_capture_ccr)?
-            falling_capture_ccr - rising_capture_ccr :
-            0xFFFF + falling_capture_ccr - rising_capture_ccr;
+        if (++quantity_of_measurements > MAX_MEASUREMENTS)
+        {
+            quantity_of_measurements = 1;
+        }
 
-    volatile unsigned int i = 0, sum = 0;
-    for (i = 0; i < MAX_MEASUREMENTS; i++)
-    {
-        sum += echo_last_measurements[i];
+        volatile unsigned int dist = compute_dist(
+                (falling_capture_ccr > rising_capture_ccr)?
+                falling_capture_ccr - rising_capture_ccr :
+                0xFFFF + falling_capture_ccr - rising_capture_ccr
+        );
+
+        dist_last_measurements[quantity_of_measurements - 1] = dist;
+
+        volatile unsigned int i = 0, sum = 0;
+        for (i = 0; i < MAX_MEASUREMENTS; i++)
+        {
+            sum += dist_last_measurements[i];
+        }
+        buzzer_play(dist_to_buzzer_freq(sum >> 4));
+        waiting_falling_edge = 0;
     }
-    buzzer_play(sum);
 }
 // ------------------------------------------------------
