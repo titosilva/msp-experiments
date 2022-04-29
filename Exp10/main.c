dfce812864a0cdd922b94a71f4f47a5b8c7f7196
titosilva <jt.mat@hotmail.com>
2022-04-28 22:28:07 -0300

    feat: working on Exp10

--- Exp10/main.c
+++ Exp10/main.c
@@ -13,7 +13,7 @@ typedef uint8_t byte;
 typedef struct {
     bool backlight_on;
     byte address;
-    byte buffer[2][16];
+    char buffer[2][16];
 } LCD;
 
 
@@ -40,24 +40,28 @@ void lcd_flush(LCD* lcd);
 
 #define LED_RED_ON      (P1OUT |= BIT0)
 #define LED_RED_OFF     (P1OUT &= ~BIT0)
-
 #define LED_GREEN_ON      (P4OUT |= BIT7)
 #define LED_GREEN_OFF     (P4OUT &= ~BIT7)
+#define MAIN_LED_TOGGLE (P2OUT ^= BIT0)
 
 void initialize_I2C_UCB0_MasterTransmitter();
 void master_TransmitOneByte(unsigned char address, unsigned char data);
 void delay_us(unsigned int time_us);
 
 volatile unsigned int measurements[4];
-volatile bool measurements_ready = false;
+volatile bool measurements_ready = 0;
 
 void configure_leds();
+void configure_buttons();
 void configure_adc_trigger_clk();
 void configure_adc();
 
-char* float_to_string(float value);
-void printVoltage(const char* name, unsigned int measurement, LCD* lcd);
-void copy_str(const char* origin, char* dest, int length);
+void millivolts_to_volt_string(int millivolts, char* result);
+void to_hex_string(int value, char* result);
+void printVoltage(char* name, unsigned int measurement, LCD* lcd);
+void copy_str(char* origin, char* dest, int length);
+
+volatile int debouncing = 0;
 
 int main(void)
 {
@@ -73,11 +77,37 @@ int main(void)
     __enable_interrupt();
 
     volatile int mode = 0;
+    volatile int sk_activated;
     while (true) {
         // Esperear até as medidas serem feitas
         while (!measurements_ready);
 
-        printVoltage("A1", measurements[0], &lcd);
+        lcd_clear(&lcd);
+
+        switch(mode + 1) {
+        case 1:
+            printVoltage("A1", measurements[0], &lcd);
+            break;
+        case 2:
+            printVoltage("A2", measurements[1], &lcd);
+            break;
+        case 3:
+            printVoltage("A3", measurements[2], &lcd);
+            break;
+        case 4:
+            printVoltage("A4", measurements[3], &lcd);
+            break;
+        }
+
+        lcd_flush(&lcd);
+        MAIN_LED_TOGGLE;
+
+        debouncing -= debouncing > 0? 5 : 0;
+        sk_activated = !(P6IN & BIT5);
+        if (sk_activated && debouncing <= 0) {
+            mode = (mode + 1) % 5;
+            debouncing = 20;
+        }
     }
 
     return 0;
@@ -90,6 +120,17 @@ void configure_leds()
 
     P4SEL &= ~BIT7;
     P4DIR |= BIT7;
+
+    P2SEL &= ~BIT0;
+    P2DIR |= BIT0;
+}
+
+void configure_buttons()
+{
+    P6SEL &= ~BIT5;
+    P6DIR &= ~BIT6;
+    P6REN |= BIT6;
+    P6OUT |= BIT6;
 }
 
 void configure_adc_trigger_clk()
@@ -144,22 +185,22 @@ void configure_adc()
 
 
     //Configurações dos canais
-    ADC12MCTL0 = ADC12SREF_0 | ADC12INCH_0;
-    ADC12MCTL1 = ADC12SREF_0 | ADC12INCH_0;
-    ADC12MCTL2 = ADC12SREF_0 | ADC12INCH_0;
-    ADC12MCTL3 = ADC12SREF_0 | ADC12INCH_0;
-    ADC12MCTL4 = ADC12SREF_0 | ADC12INCH_1;
-    ADC12MCTL5 = ADC12SREF_0 | ADC12INCH_1;
-    ADC12MCTL6 = ADC12SREF_0 | ADC12INCH_1;
-    ADC12MCTL7 = ADC12SREF_0 | ADC12INCH_1;
-    ADC12MCTL8 = ADC12SREF_0 | ADC12INCH_2;
-    ADC12MCTL9 = ADC12SREF_0 | ADC12INCH_2;
-    ADC12MCTL10 = ADC12SREF_0 | ADC12INCH_2;
-    ADC12MCTL11 = ADC12SREF_0 | ADC12INCH_2;
-    ADC12MCTL12 = ADC12SREF_0 | ADC12INCH_3;
-    ADC12MCTL13 = ADC12SREF_0 | ADC12INCH_3;
-    ADC12MCTL14 = ADC12SREF_0 | ADC12INCH_3;
-    ADC12MCTL15 = ADC12SREF_0 | ADC12INCH_3 | ADC12EOS;
+    ADC12MCTL0 = ADC12SREF_0 | ADC12INCH_1;
+    ADC12MCTL1 = ADC12SREF_0 | ADC12INCH_1;
+    ADC12MCTL2 = ADC12SREF_0 | ADC12INCH_1;
+    ADC12MCTL3 = ADC12SREF_0 | ADC12INCH_1;
+    ADC12MCTL4 = ADC12SREF_0 | ADC12INCH_2;
+    ADC12MCTL5 = ADC12SREF_0 | ADC12INCH_2;
+    ADC12MCTL6 = ADC12SREF_0 | ADC12INCH_2;
+    ADC12MCTL7 = ADC12SREF_0 | ADC12INCH_2;
+    ADC12MCTL8 = ADC12SREF_0 | ADC12INCH_3;
+    ADC12MCTL9 = ADC12SREF_0 | ADC12INCH_3;
+    ADC12MCTL10 = ADC12SREF_0 | ADC12INCH_3;
+    ADC12MCTL11 = ADC12SREF_0 | ADC12INCH_3;
+    ADC12MCTL12 = ADC12SREF_0 | ADC12INCH_4;
+    ADC12MCTL13 = ADC12SREF_0 | ADC12INCH_4;
+    ADC12MCTL14 = ADC12SREF_0 | ADC12INCH_4;
+    ADC12MCTL15 = ADC12SREF_0 | ADC12INCH_4 | ADC12EOS;
 
     // Habilita interrupção na última conversão
     ADC12IE = ADC12IE15;
@@ -279,8 +320,8 @@ void lcd_flush(LCD* lcd)
 {
     volatile int i, j;
     for (i = 0; i < 2; i++) {
+        lcd_set_cursor_position(lcd, i, 0);
         for (j = 0; j < 16; j++) {
-            lcd_set_cursor_position(lcd, i, j);
             lcd_write_char(lcd, lcd->buffer[i][j]);
         }
     }
@@ -341,47 +382,70 @@ void delay_us(unsigned int time_us)
     TA1CTL = MC_0 | TACLR;
 }
 
-char* float_to_string(float value)
+void millivolts_to_volt_string(int millivolts, char* result)
 {
-    volatile int expanded = (int)(value * 1000);
-    volatile int currentDivisor = 10;
-    volatile char result[5];
-
     volatile int currentIndex;
-    for(currentIndex = 4; currentIndex >= 0; currentIndex++) {
+    volatile int digit = 0;
+    for(currentIndex = 4; currentIndex >= 0; currentIndex--) {
         if (currentIndex == 1) {
             result[currentIndex] = ',';
             continue;
         }
 
-        result[currentIndex] = (expanded % currentDivisor) + '0';
-        expanded = (expanded - (expanded % currentDivisor))/10;
+        digit = (millivolts % 10);
+        result[currentIndex] =  digit + '0';
+        millivolts = (millivolts - digit)/10;
     }
-
-    return result;
 }
 
-void printVoltage(const char* name, unsigned int measurement, LCD* lcd)
+void to_hex_string(int value, char* result)
 {
-    volatile float voltage = ((float)measurement / 255) * 3.3;
-    volatile char* str = float_to_string(voltage);
+    volatile int currentIndex;
+    volatile int digit = 0;
+    for (currentIndex = 3; currentIndex >= 0; currentIndex--) {
+        digit = (value % 16);
+        if (digit < 10) {
+            result[currentIndex] =  digit + '0';
+        } else {
+            result[currentIndex] = digit - 10 + 'a';
+        }
+        value = (value - digit) / 16;
+    }
+}
 
-    lcd_clear(lcd);
+void printVoltage(char* name, unsigned int measurement, LCD* lcd)
+{
+    // Arredondamento: 1000 * 3,3 / 255 = 12.94 ~ (13 - 1/16)
+    volatile int millivolts = measurement * 13 - (measurement >> 4);
+    char voltage_str[5];
+    millivolts_to_volt_string(millivolts, voltage_str);
+    char hex_str[4];
+    to_hex_string(measurement, hex_str);
 
-    copy_str(name, lcd->buffer[0], 2);
-
+    lcd->buffer[0][0] = name[0];
+    lcd->buffer[0][1] = name[1];
     lcd->buffer[0][2] = '=';
 
-    copy_str(str, lcd->buffer[0] + 3, 5);
+    lcd->buffer[0][3] = voltage_str[0];
+    lcd->buffer[0][4] = voltage_str[1];
+    lcd->buffer[0][5] = voltage_str[2];
+    lcd->buffer[0][6] = voltage_str[3];
+    lcd->buffer[0][7] = voltage_str[4];
     lcd->buffer[0][8] = 'V';
 
-    lcd_flush(lcd);
+    lcd->buffer[0][9] = ' ';
+    lcd->buffer[0][10] = '0';
+    lcd->buffer[0][11] = 'x';
+    lcd->buffer[0][12] = hex_str[0];
+    lcd->buffer[0][13] = hex_str[1];
+    lcd->buffer[0][14] = hex_str[2];
+    lcd->buffer[0][15] = hex_str[3];
 }
 
-void copy_str(const char* origin, char* dest, int length) {
+void copy_str(char* origin, char* dest, int length) {
     volatile int i = 0;
     for (i = 0; i++; i < length) {
-        dest[i] = origin[i];
+        *(dest + i) = *(origin + i);
     }
 }
 
@@ -414,5 +478,3 @@ __interrupt void __adc12_interrupt(void)
             break;
     }
 }
-
-
